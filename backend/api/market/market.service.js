1575a292eaf5deda3df7f676856c7436f16d6bbb
zivtal <zivtal83@gmail.com>
2021-12-15 15:39:23 +0200

    market service

--- backend/api/market/market.service.js
+++ backend/api/market/market.service.js
@@ -6,12 +6,8 @@ const collectionDb = 'forms';
 
 async function query(query) {
     try {
-        const sort = _buildSortCriteria(query);
-        const filter = _buildFilterCriteria(query);
         const collection = await dbService.getCollection(collectionDb);
-        let forms = await collection.find(filter).sort(sort);
-        forms = await forms.toArray();
-        return forms;
+        return await collection.find().toArray();
     } catch (err) {
         logger.error(`failed to get forms from '${collectionDb}':`, err);
         throw err
@@ -27,26 +23,6 @@ async function add(form) {
         logger.error('adding failed:', err);
         throw err;
     }
-}
-
-function _buildSortCriteria({ sort }) {
-    const criteria = {};
-    if (sort) {
-        sort = JSON.parse(sort);
-        criteria[sort.type] = (sort.descending) ? -1 : 1;
-    }
-    return criteria;
-}
-
-function _buildFilterCriteria({ filter }) {
-    const criteria = {};
-    if (filter) {
-        filter = JSON.parse(filter);
-        if (filter.name) criteria.name = { $regex: filter.name, $options: 'i' };
-        if (filter.tags?.length) criteria.tags = { $elemMatch: { $regex: `^.*(${filter.tags.join('|')}).*$`, $options: 'i' } }
-        if (filter.inStock !== null) criteria.inStock = { inStock: { $eq: filter.inStock } };
-    }
-    return criteria;
 }
 
 module.exports = {
